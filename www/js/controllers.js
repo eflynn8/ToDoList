3974dff02c63b4abc0599e7e58dc3ce82c3cfb74
eflynn8 <elf828@gmail.com>
2017-01-21 20:45:21 -0500

    More task suggestions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,13 +23,44 @@ todoApp.controllers = {
               .then(function(value) {
                   todoApp.models.todo.add(value);
                   if (value == "Cereal" || value == "cereal") {
-                          ons.notification.confirm({message: 'Would you like to add "milk"?'}).then(function(value){
+                          ons.notification.confirm({message: 'Would you like to add "Milk" to your list?'}).then(function(value){
                              if (value == 1) {
                                 todoApp.models.todo.add("Milk");
                                 todoApp.views.mainPage(page);
                               }
                           });
-                     
+                  }
+                  else if (value == "Peanut butter" || value == "peanut butter") {
+                          ons.notification.confirm({message: 'Would you like to add "Jelly" to your list?'}).then(function(value){
+                             if (value == 1) {
+                                todoApp.models.todo.add("Jelly");
+                                todoApp.views.mainPage(page);
+                              }
+                          });
+                  }
+                  else if (value != "Ice cream" || value != "ice cream") {
+                          ons.notification.confirm({message: 'Would you like to add "Ice cream" to your list?'}).then(function(value){
+                             if (value == 1) {
+                                todoApp.models.todo.add("Ice cream");
+                                todoApp.views.mainPage(page);
+                              }
+                          });
+                  }
+                  else if (value == "Sugar" || value == "sugar") {
+                          ons.notification.confirm({message: 'Would you like to add "Eggs" to your list?'}).then(function(value){
+                             if (value == 1) {
+                                todoApp.models.todo.add("Eggs");
+                                todoApp.views.mainPage(page);
+                              }
+                          });
+                  }
+                  else if (value == "Homework" || value == "homework") {
+                          ons.notification.confirm({message: 'Would you like to add "Procrastinate" to your list?'}).then(function(value){
+                             if (value == 1) {
+                                todoApp.models.todo.add("Procrastinate");
+                                todoApp.views.mainPage(page);
+                              }
+                          });
                   }
                   todoApp.views.mainPage(page);
               });
